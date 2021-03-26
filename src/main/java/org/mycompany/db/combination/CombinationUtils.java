a1d81218a702efc9c29638171d492804eaadc29f
GalinaIs <denis@isyanov.com>
2021-03-26 14:30:05 +0300

    Add get all combinations from word's letters using combinatoricslib3.

---
+++ src/main/java/org/mycompany/db/combination/CombinationUtils.java
@@ -0,0 +1,33 @@
+package org.mycompany.db.combination;
+
+import org.mycompany.db.letter.LettersArray;
+import org.paukov.combinatorics3.Generator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CombinationUtils {
+    private CombinationUtils() {
+        throw new AssertionError("No instance!");
+    }
+
+    static List<LettersArray> getCombinations(List<Character> letters, int k) {
+        return Generator.combination(letters)
+                .simple(k)
+                .stream()
+                .map(LettersArray::new)
+                .collect(Collectors.toList());
+    }
+
+    public static List<LettersArray> getAllCombinations(String word) {
+        List<LettersArray> combinations = new ArrayList<>();
+        List<Character> letters = word.chars()// Convert to an IntStream
+                .mapToObj(i -> (char) i) // Convert int to char, which gets boxed to Character
+                .collect(Collectors.toList());
+        for (int i = 1; i <= word.length(); i++) {
+            combinations.addAll(getCombinations(letters, i));
+        }
+        return combinations;
+    }
+}
---
+++ src/test/java/org/mycompany/db/combination/CombinationUtilsTest.java
@@ -0,0 +1,24 @@
+package org.mycompany.db.combination;
+
+import org.assertj.core.util.Lists;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mycompany.db.letter.LettersArray;
+
+import java.util.List;
+
+public class CombinationUtilsTest {
+    @Test
+    public void test() {
+        List<LettersArray> allCombinations = CombinationUtils.getAllCombinations("123");
+        Assert.assertEquals(7, allCombinations.size());
+        Assert.assertTrue(allCombinations.contains(new LettersArray(Lists.list('1'))));
+        Assert.assertTrue(allCombinations.contains(new LettersArray(Lists.list('2'))));
+        Assert.assertTrue(allCombinations.contains(new LettersArray(Lists.list('3'))));
+        Assert.assertTrue(allCombinations.contains(new LettersArray(Lists.list('1', '2'))));
+        Assert.assertTrue(allCombinations.contains(new LettersArray(Lists.list('1', '3'))));
+        Assert.assertTrue(allCombinations.contains(new LettersArray(Lists.list('2', '3'))));
+        Assert.assertTrue(allCombinations.contains(new LettersArray(Lists.list('1', '2', '3'))));
+    }
+
+}
